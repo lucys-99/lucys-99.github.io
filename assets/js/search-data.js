5502d735d3176913020f3af1f4881a9b4f636dd1
lucys-99 <56526798+lucys-99@users.noreply.github.com>
2025-01-11 00:49:34 +0000

    Deploying to gh-pages from @ lucys-99/lucys-99.github.io@2d6d1eafa823cc734fe55986609556822b2a997b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-image-galleries",
       
